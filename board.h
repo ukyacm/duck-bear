d97ac423122ffea6ad6d9dd2bb0cedb0c4bd054e
Connor Greenwell <cgree3@gmail.com>
2013-07-21 14:42:16 -0400

    added board header

--- board.h
+++ board.h
@@ -1,13 +1,18 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <vector>
 #include <string>
+
 
 enum Piece {
 	NONE,
 	WHITE,
 	BLACK
 };
+
+typedef std::vector<Piece> Row;
+typedef std::vector<Row> Grid;
 
 class IllegalMoveException : public std::exception {
 	public:
@@ -18,13 +23,17 @@ class IllegalMoveException : public std::exception {
 
 class Board {
 	public:
-	Piece board[9][9];
+	const int BOARD_SIZE = 9;
+
+	Grid curr, prev, prever;
 	
 	Board();
 	~Board();
 
 	void place(const int x, const int y);
 	Piece at(const int x, const int y) const;
+
+	void resolve();
 };
 
 #endif
